dfd1a83fb3758c76c03ce15bb6f9b3b1ddd0321b
jeevana1234 <maddelajeevana2003@gmail.com>
2024-03-26 17:35:03 +0530

    fineshed

---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return (
+    <div>
+      <div className="background-image-container">
+        <div className="background-image"></div>
+        <div className="overlay"></div>
+        <div className="text">
+        Empowering Efficiency, Enabling Success: Streamlining Your Projects with Precision and Purpose.
+        </div>
+      </div>
+
+      <footer className="footer">
+      <div className="footer-content">
+          <p>&copy; 2024 Your Company Name. All rights reserved.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
+
